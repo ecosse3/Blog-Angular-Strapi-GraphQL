3b8c3d7f838f02927956efd8800de9ccb24f74ee
Łukasz Kurpiewski <theecosse@gmail.com>
2020-04-03 18:42:01 +0200

    Added logic to article component

--- frontend/src/app/article/article.component.ts
+++ frontend/src/app/article/article.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import ARTICLE_QUERY from '../apollo/queries/article/article';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.scss']
+  styleUrls: ['./article.component.scss'],
 })
-export class ArticleComponent implements OnInit {
+export class ArticleComponent implements OnInit, OnDestroy {
+  data: any = {};
+  loading = true;
+  errors: any;
 
-  constructor() { }
+  private queryArticle: Subscription;
 
-  ngOnInit(): void {
+  constructor(private apollo: Apollo, private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.queryArticle = this.apollo
+      .watchQuery({
+        query: ARTICLE_QUERY,
+        variables: {
+          id: this.route.snapshot.paramMap.get('id'),
+        },
+      })
+      .valueChanges.subscribe((result) => {
+        this.data = result.data;
+        this.loading = result.loading;
+        this.errors = result.errors;
+      });
   }
-
+  ngOnDestroy() {
+    this.queryArticle.unsubscribe();
+  }
 }
